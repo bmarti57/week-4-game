d6d0dbf99e0d263c87ce03dfb9525268de8b92da
Brian Martin <Brian Martin>
2017-07-30 21:46:05 -0400

    html and js linked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,20 @@ $(document).ready(function() {
 
 	var wins = 0;
 	var losses = 0;
-	var userScore = 0;
+	var userScore= 0;
+
+	var randomNumber = Math.floor(Math.random() * 102) + 19;
+		console.log(randomNumber);
+
+	var blue = Math.floor(Math.random() * 12) + 1;
+	var red = Math.floor(Math.random() * 12) + 1;
+	var green = Math.floor(Math.random() * 12) + 1;
+	var pink = Math.floor(Math.random() * 12) + 1;
 
 
-
+		$("#compNumber").html("Random Number: " + randomNumber);
+		$("#yourScore").html("Your Score: " + userScore);
+		$("#win").html("Wins: " + wins);
+		$("#loss").html("Losses: " + losses);
 
 });//document ready
