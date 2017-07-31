1f4bdb1986af4635a6b01b2a5e7160a9aa031b5c
Brian Martin <Brian Martin>
2017-07-30 23:00:53 -0400

    js mostly done, need to add crystal value to score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,14 +8,65 @@ $(document).ready(function() {
 		console.log(randomNumber);
 
 	var blue = Math.floor(Math.random() * 12) + 1;
+		console.log(blue);
 	var red = Math.floor(Math.random() * 12) + 1;
+		console.log(red);
 	var green = Math.floor(Math.random() * 12) + 1;
+		console.log(green);
 	var pink = Math.floor(Math.random() * 12) + 1;
+		console.log(pink);
 
-
+	//Displayed in the html
 		$("#compNumber").html("Random Number: " + randomNumber);
 		$("#yourScore").html("Your Score: " + userScore);
 		$("#win").html("Wins: " + wins);
 		$("#loss").html("Losses: " + losses);
+
+	//Click Function for Crystals
+		$("#sapphire").on("click", function() {
+			userScore = blue = userScore;
+		});// Sapphire 
+		$("#ruby").on("click", function() {
+			userScore = red = userScore;
+		});// Ruby 
+		$("#emerald").on("click", function() {
+			userScore = green = userScore;
+		});// Emerald 		
+		$("#diamond").on("click", function() {
+			userScore = pink = userScore;
+		});// Diamond 
+
+	//If Statements
+		if (userScore = randomNumber) {
+			wins++;
+			var userScore = 0;
+			var randomNumber = Math.floor(Math.random() * 102) + 19;
+				console.log(randomNumber);
+			var blue = Math.floor(Math.random() * 12) + 1;
+				console.log(blue);
+			var red = Math.floor(Math.random() * 12) + 1;
+				console.log(red);
+			var green = Math.floor(Math.random() * 12) + 1;
+				console.log(green);
+			var pink = Math.floor(Math.random() * 12) + 1;
+				console.log(pink);
+			console.log("Wins: " + wins + "Losses: " + losses);
+		}
+
+		if (userScore > randomNumber) {
+			losses++;
+			var userScore = 0;
+			var randomNumber = Math.floor(Math.random() * 102) + 19;
+				console.log(randomNumber);
+			var blue = Math.floor(Math.random() * 12) + 1;
+				console.log(blue);
+			var red = Math.floor(Math.random() * 12) + 1;
+				console.log(red);
+			var green = Math.floor(Math.random() * 12) + 1;
+				console.log(green);
+			var pink = Math.floor(Math.random() * 12) + 1;
+				console.log(pink);
+			console.log("Wins: " + wins + "Losses: " + losses);
+		}
 
 });//document ready
