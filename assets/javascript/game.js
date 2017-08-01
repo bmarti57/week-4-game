d6827243779509920f340033c3fc7e981d45d862
Brian Martin <Brian Martin>
2017-07-31 22:11:28 -0400

    final push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,81 +1,63 @@
 $(document).ready(function() {
-
+	
+	var randomNumber = Math.floor(Math.random() * 102) + 19;
 	var wins = 0;
 	var losses = 0;
-	var userScore = 0;
-		console.log("User Score: " + userScore);
-
-	var randomNumber = Math.floor(Math.random() * 102) + 19;
-		console.log("Random Number: " + randomNumber);
-
 	var blue = Math.floor(Math.random() * 12) + 1;
-		console.log("Sapphire: " + blue);
 	var red = Math.floor(Math.random() * 12) + 1;
-		console.log("Ruby: " + red);
 	var green = Math.floor(Math.random() * 12) + 1;
-		console.log("Emerald: " + green);
 	var pink = Math.floor(Math.random() * 12) + 1;
-		console.log("Diamond: " + pink);
+	var userScore = 0;
+
+			function crystalNumber() {
+				blue = Math.floor(Math.random() * 12) + 1;
+				red = Math.floor(Math.random() * 12) +1;
+				green = Math.floor(Math.random() * 12) +1;
+				pink = Math.floor(Math.random() * 12) +1;
+			}
+
+			function winLose() {
+				if (userScore === randomNumber) {
+					wins++;
+					userScore = 0;
+					$("#win").html("Wins: " + wins);
+					randomNumber = Math.floor(Math.random() * 102) + 19;
+					$("#compNumber").html("Random Number: " + randomNumber);
+										crystalNumber();
+				}
+				else if (userScore > randomNumber) {
+					losses++;
+					userScore = 0;
+					$("#loss").html("Losses: " + losses);
+					randomNumber = Math.floor(Math.random() * 102) + 19;
+					$("#compNumber").html("Random Number: " + randomNumber);
+					crystalNumber();
+				}
+			}
+
+		$("#compNumber").html("Random Number: " + randomNumber);
 
 	//Click Function for Crystals
 		$("#sapphire").on("click", function() {
-				userScore = parseInt(userScore, 10) + parseInt(blue, 10);
+				userScore = userScore + blue;
 				$("#yourScore").html("Your Score: " + userScore);
-			console.log("New User Score: " + userScore);
-		});// Sapphire 
+				winLose();		
+		});
 		$("#ruby").on("click", function() {
-				userScore = parseInt(userScore, 10) + parseInt(red, 10);	
+				userScore = userScore + red;
 				$("#yourScore").html("Your Score: " + userScore);
-			console.log("New User Score: " + userScore);
-		});// Ruby 
+				winLose();		
+		});
 		$("#emerald").on("click", function() {
-				userScore = parseInt(userScore, 10) + parseInt(green, 10);
+				userScore = userScore + green;
 				$("#yourScore").html("Your Score: " + userScore);
-			console.log("New User Score: " + userScore);
-		});// Emerald 		
+				winLose();
+		}); 		
 		$("#diamond").on("click", function() {
-				userScore = parseInt(userScore, 10) + parseInt(pink, 10);	
+				userScore = userScore + pink;	
 				$("#yourScore").html("Your Score: " + userScore);
-			console.log("New User Score: " + userScore);
-		});// Diamond 
+				winLose();
+		});
 
-	//If Statements
-		if (userScore == randomNumber) {
-			wins++;
-			var userScore = 0;
-			var randomNumber = Math.floor(Math.random() * 102) + 19;
-				console.log(randomNumber);
-			var blue = Math.floor(Math.random() * 12) + 1;
-				console.log(blue);
-			var red = Math.floor(Math.random() * 12) + 1;
-				console.log(red);
-			var green = Math.floor(Math.random() * 12) + 1;
-				console.log(green);
-			var pink = Math.floor(Math.random() * 12) + 1;
-				console.log(pink);
-			console.log("Wins: " + wins + "Losses: " + losses);
-		}
-
-		else if (userScore > randomNumber) {
-			losses++;
-			var userScore = 0;
-			var randomNumber = Math.floor(Math.random() * 102) + 19;
-				console.log(randomNumber);
-			var blue = Math.floor(Math.random() * 12) + 1;
-				console.log(blue);
-			var red = Math.floor(Math.random() * 12) + 1;
-				console.log(red);
-			var green = Math.floor(Math.random() * 12) + 1;
-				console.log(green);
-			var pink = Math.floor(Math.random() * 12) + 1;
-				console.log(pink);
-			console.log("Wins: " + wins + "Losses: " + losses);
-		}
-
-		//Displayed in the html
-		$("#compNumber").html("Random Number: " + randomNumber);
-		$("#yourScore").html("Your Score: " + userScore);
-		$("#win").html("Wins: " + wins);
-		$("#loss").html("Losses: " + losses);
 
 });//document ready
